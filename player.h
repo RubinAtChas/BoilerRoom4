de7a8772fd2be6450c71e3905eb05ffab61dab8b
RubinAtChas <jonathan.rubin@chasacademy.se>
2024-11-11 11:36:17 +0100

    Updated Class Player

--- player.h
+++ player.h
@@ -2,13 +2,34 @@
 #define PLAYER_H
 #include <string>
 #include <vector>
+#include <iostream>
 #include "Card.h"
 
 class Player
 {
-public:
+private:
 std::string name;
 std::vector<Card>hand;
+public:
+Player(std::string name, std::vector<Card>hand)
+: name(name),hand(hand){}
+
+void displayName()
+{
+    std::cout << name << std::endl;
+
+}
+Card playCard()
+{
+    Card lastCard = hand.back();
+    hand.pop_back();
+    return lastCard;
+}
+void addCards(std::vector<Card>playedCards)
+{
+    hand.insert(hand.begin(),playedCards.begin(),playedCards.end());
+}
+
 };
 
 #endif
